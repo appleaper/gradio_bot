41e02d7c927e496d6fbe0bfe3974b4770312413b
APP <1047914937@qq.com>
2025-04-09 20:04:44 +0800

    看看那些是要保留的

--- local/voice/voice_model_init.py
+++
@@ -1,9 +0,0 @@
-from local.rag.parse.fireredasr.models.fireredasr import FireRedAsr
-
-def load_voice_model(model_name):
-    if model_name == 'FireRedAsr':
-        model = FireRedAsr.from_pretrained("aed", name2path['FireRedAsr'])
-        tokenizer = None
-        return model, tokenizer
-    else:
-        assert False, 'voice model not support!'
